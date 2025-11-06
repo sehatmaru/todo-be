ecd85169f17309499265c66cebfff7d85f0e3d5a
sehatmaru <maruzhaky@gmail.com>
2025-11-06 15:08:40 +0700

    ssl

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,18 @@
 import { Pool } from "pg";
 import "./env";
 
+const useSSL = (() => {
+    if (typeof process.env.POSTGRES_SSL === "string") {
+        return process.env.POSTGRES_SSL.toLowerCase() === "true";
+    }
+    return process.env.NODE_ENV === "production";
+})();
+
 export const db = new Pool({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : 5432,
+    ssl: useSSL ? { rejectUnauthorized: false } : undefined,
 });
